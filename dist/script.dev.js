b1c6760f2a89c595bebcda070cffd5c1093d002c
Raul Macêdo De Sena Neto <149539929+Rauldograu7@users.noreply.github.com>
2024-01-30 10:49:29 -0300

    subi os arquivos do meu site

---
+++ dist/script.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var show = true;
+var menuContent = document.querySelector('.content');
+var menuToggle = menuContent.querySelector('.menu-toggle');
+menuToggle.addEventListener('click', function () {
+  document.body.style.overflow = show ? 'hidden' : 'initial';
+  menuContent.classList.toggle('on', show);
+  show = !show;
+});
+//# sourceMappingURL=script.dev.js.map
